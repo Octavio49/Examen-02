aff549f74246e350ca13fc839d7717a614573b3a
Octavio <deavila.octavio23@gmail.com>
2026-05-03 20:20:32 -0600

    add production server

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'JWT')
     .addTag('Cursos')
     .addServer("http://localhost:3000", "Servidor local")
-    .addServer("https://practica-iii-oidr.onrender.com", "Servidor de produccion")
+    .addServer("https://examen-02-n0u2.onrender.com", "Servidor de produccion")
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
